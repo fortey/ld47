aa84e3e1f04e7f805450e8d5ee4291d2bec25977
fortey <fortey@inbox.ru>
2020-10-03 16:22:11 +0700

    added boss

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    public float startTimeBtwShoots;
+
+    float timeBtwShoots;
+    void Start()
+    {
+        timeBtwShoots = startTimeBtwShoots;
+    }
+
+
+    void Update()
+    {
+        if (timeBtwShoots <= 0)
+        {
+            Instantiate(Bullet, transform.position, Quaternion.identity);
+            timeBtwShoots = startTimeBtwShoots;
+        }
+        else
+        {
+            timeBtwShoots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 screenRes;
+    private float objectWidth;
+    private float objectHeight;
+    void Start()
+    {
+        screenRes = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+
+        rb.velocity = transform.up * Speed;
+    }
+
+    void FixedUpdate()
+    {
+        if (-screenRes.x - objectWidth > transform.position.x || screenRes.x + objectWidth < transform.position.x
+            || -screenRes.y - objectHeight > transform.position.y || screenRes.y + objectHeight < transform.position.y)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
