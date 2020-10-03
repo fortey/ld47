1ba17e2858cf567b85129ffc1f962b442b3fd9ef
fortey <fortey@inbox.ru>
2020-10-04 01:23:16 +0700

    added spawn clocks

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : Enemy
+{
+    public float AddedTime = 10f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            onDead();
+
+            Destroy(gameObject);
+        }
+    }
+}
