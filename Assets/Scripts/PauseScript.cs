ce608d53117221ab3da5941fd057f6e975624929
TheRedBeast <anna.bulavina.02@gmail.com>
2020-10-03 20:59:47 +0300

    NewPauseMenu

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -5,17 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
+    public GameObject menu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+            menu.SetActive(true);
+        }
+    }
     public void GoToMenu()
     {
         SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
     }
 
     public void PauseGame()
     {
         Time.timeScale = 0;
+
     }
     public void ContinueGame()
     {
         Time.timeScale = 1;
+       
     }
 }
