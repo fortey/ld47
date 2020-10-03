5d09dfd67974750742ec4619f8b3b08b74f75d59
fortey <fortey@inbox.ru>
2020-10-03 15:19:08 +0700

    added moving

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+    private Vector2 screenRes;
+    private float objectWidth;
+    private float objectHeight;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        screenRes = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
+
+    void Update()
+    {
+        var moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveVector.normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+
+    }
+
+    private void LateUpdate()
+    {
+        var board = transform.position;
+        board.x = Mathf.Clamp(board.x, -screenRes.x + objectWidth, screenRes.x - objectWidth);
+        board.y = Mathf.Clamp(board.y, -screenRes.y + objectHeight, screenRes.y - objectHeight);
+        transform.position = board;
+    }
+}
