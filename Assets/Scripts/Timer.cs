974fbdb0d74fc6087993d70309e7f64cae7ff6f4
TheRedBeast <anna.bulavina.02@gmail.com>
2020-10-03 21:41:17 +0300

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float StartTime;
+    public Text time;
+    private float newtime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time.text = StartTime.ToString();
+        newtime = StartTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartTime -= Time.deltaTime;
+        
+        if (StartTime < 0)
+        {
+            StartTime = newtime - 5;
+            time.text = StartTime.ToString();
+            newtime = StartTime;
+        }
+        else
+            time.text = StartTime.ToString("#");
+    }
+}
