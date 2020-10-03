66ae40b2db84e86a1a88e84e0305492945fb1118
TheRedBeast <anna.bulavina.02@gmail.com>
2020-10-03 18:58:45 +0300

    audioSettings

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer am;
+    
+    public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("MasterVolume", sliderValue);
+    }
+}
