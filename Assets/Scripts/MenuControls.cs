1d50f374fc72f2646c05cd06ae3965b5de884fcd
TheRedBeast <anna.bulavina.02@gmail.com>
2020-10-03 20:49:10 +0300

    ScenesConnection

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
+    public AudioSource audsor;
     public void PlayPressed()
     {
         SceneManager.LoadScene("SampleScene");
+        DontDestroyOnLoad(audsor);
     }
     public void ExitPressed()
     {
         Application.Quit();
     }
+
+    
 }
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+    }
+}
