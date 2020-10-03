4a6164e931b4195570be06347a838733b7439ca1
fortey <fortey@inbox.ru>
2020-10-03 19:58:56 +0700

    added passive enemy

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject passiveEnemy;
+    public float passiveEnemyTime = 1f;
+
+    private Vector2 screenRes;
+
+    private void Start()
+    {
+        screenRes = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(Spawn(passiveEnemy));
+    }
+
+    private IEnumerator Spawn(GameObject enemy)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(passiveEnemyTime);
+            var x = Random.Range(-screenRes.x, screenRes.x);
+            var y = Random.Range(-screenRes.y, screenRes.y);
+
+            Instantiate(passiveEnemy, new Vector3(x, y, 0), Quaternion.identity);
+        }
+    }
+}
