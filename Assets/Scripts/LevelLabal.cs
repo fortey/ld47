af81203b72683fe9cdf3350547132349385f06ff
fortey <fortey@inbox.ru>
2020-10-05 01:20:47 +0700

    label

---
+++ Assets/Scripts/LevelLabal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLabal : MonoBehaviour
+{
+    public GameObject Good;
+    public GameObject Bad;
+
+    void Start()
+    {
+
+    }
+
+    public void ShowBad()
+    {
+        Good.SetActive(true);
+        StartCoroutine(HideLevelLabel());
+    }
+
+    private IEnumerator HideLevelLabel()
+    {
+        yield return new WaitForSeconds(1);
+        Good.SetActive(false);
+        Bad.SetActive(false);
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,11 +10,14 @@ public class LevelManager : MonoBehaviour
     public string LevelName;
     public string NextScene;
 
+    LevelLabal label;
+
     void Start()
     {
         LevelLabel.text = LevelName;
-        LevelLabel.gameObject.SetActive(true);
-        StartCoroutine(HideLevelLabel());
+        //LevelLabel.gameObject.SetActive(true);
+        //StartCoroutine(HideLevelLabel());
+        label = GameObject.FindObjectOfType<LevelLabal>();
 
         var character = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
         character.onDead += () => EndLevel(character);
@@ -32,17 +35,31 @@ public class LevelManager : MonoBehaviour
         {
             if (GlobalVariables.instance)
             {
-                GlobalVariables.instance.SetNext();
-                GlobalVariables.instance.Score++;
-                SceneManager.LoadScene(GlobalVariables.instance.NextScene);
+                StartCoroutine(SwitchLevelGood());
             }
 
         }
         else
         {
-            if (GlobalVariables.instance)
-                GlobalVariables.instance.Score--;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            StartCoroutine(SwitchLevelBad());
         }
+    }
+
+    private IEnumerator SwitchLevelGood()
+    {
+        label.Good.SetActive(true);
+        yield return new WaitForSeconds(1);
+        GlobalVariables.instance.SetNext();
+        GlobalVariables.instance.Score++;
+        SceneManager.LoadScene(GlobalVariables.instance.NextScene);
+    }
+
+    private IEnumerator SwitchLevelBad()
+    {
+        label.Bad.SetActive(true);
+        yield return new WaitForSeconds(1);
+        if (GlobalVariables.instance)
+            GlobalVariables.instance.Score--;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
